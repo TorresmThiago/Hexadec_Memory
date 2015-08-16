fc904f1919b525ffd855e656fb65e59644ddde8a
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-16 20:55:49 -0300

    Twist and..
    Yeah, it kinda does not shout yet. Our problem is that it does not
    render all the process of the rotation. Come to think of it, just shout,
    no twist.

--- Assets/RotationScript.cs
+++ Assets/RotationScript.cs
@@ -4,7 +4,25 @@ using System.Collections;
 
 public class RotationScript : MonoBehaviour {
 
-	public void HelloWord (GameObject btn) {
+	/*public void HelloWord (GameObject btn) {
 		print (btn.GetComponentInChildren<Text>().text);
+	}*/
+
+	IEnumerator FixAngle(GameObject toFix){
+		switch ((int)toFix.transform.localEulerAngles.y) {
+			case 179:
+				toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
+				break;
+		}
+
+		yield return new WaitForSeconds (0.23f);
+	}
+
+	public void Move(GameObject btn){
+		while (btn.transform.localEulerAngles.y <= 180) {
+			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
+			btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
+			StartCoroutine(FixAngle(btn));
+		}
 	}
 }
