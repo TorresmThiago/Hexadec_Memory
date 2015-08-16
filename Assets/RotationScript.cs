44290eecd4a7276146e5f78d639d2f53a33799ce
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-16 18:59:20 -0300

    Accessing Buttons.
    We know now how to use their texts to store their colors, and how to
    access buttons to functions.

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+
+	public void HelloWord (GameObject btn) {
+		print (btn.GetComponentInChildren<Text>().text);
+	}
+}
