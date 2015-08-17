504d55dd41d957ced205b6172973562b7ebaf97c
Thiago Torres <TheThiagoo0@gmail.com>
2015-08-17 14:36:50 -0300

    Bit by bit
    It does turn back, after a little while. But at least, you've got the
    function to make it turn back at your own will. Put this with the
    choosing mechanic .o/

--- Assets/RotationScript.cs
+++ Assets/RotationScript.cs
@@ -4,23 +4,27 @@ using System.Collections;
 
 public class RotationScript : MonoBehaviour {
 
-	/*public void HelloWord (GameObject btn) {
-		print (btn.GetComponentInChildren<Text>().text);
-	}*/
-
-	private bool isRotating = false;
+	private string isRotating = null;
 	private GameObject card;
 
 	IEnumerator FixAngle(GameObject toFix){
 		switch ((int)toFix.transform.localEulerAngles.y) {
 			case 179:
 				toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
-				isRotating = false;
+				isRotating = null;
+				yield return new WaitForSeconds (2f);
+				isRotating = "Back";
+				break;
+
+			case 359:
+				toFix.transform.rotation = Quaternion.Euler (0, 0, 0);
+				print("Imma here?");
+				isRotating = null;
 				card = null;
 				break;
 		}
 
-		yield return new WaitForSeconds (1f);
+
 	}
 
 	public void Move(GameObject btn){
@@ -29,14 +33,22 @@ public class RotationScript : MonoBehaviour {
 		StartCoroutine(FixAngle(btn));
 	}
 
+	public void MoveBack(GameObject btn){
+		Quaternion newRotation = Quaternion.AngleAxis (0, Vector3.up);
+		btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
+		StartCoroutine(FixAngle(btn));
+	}
+
 	public void Change(GameObject btn){
 		card = btn;
-		isRotating = true;
+		isRotating = "Front";
 	}
 
 	void Update(){
-		if(isRotating){
+		if(isRotating == "Front"){
 			Move(card);
+		} else if(isRotating == "Back"){
+			MoveBack(card);
 		}
 	}
 }
