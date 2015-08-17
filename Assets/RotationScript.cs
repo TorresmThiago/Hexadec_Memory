ae5d85f3f6bfdec0ee66687309f9f3aaf08bc162
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-16 21:44:46 -0300

    And we've got a card!
    That basic rotation. Next thing to do: Implement the choosing wrong
    cards mechanic, where you pick two cards, wait a little bit and they
    rotate back.

--- Assets/RotationScript.cs
+++ Assets/RotationScript.cs
@@ -8,21 +8,35 @@ public class RotationScript : MonoBehaviour {
 		print (btn.GetComponentInChildren<Text>().text);
 	}*/
 
+	private bool isRotating = false;
+	private GameObject card;
+
 	IEnumerator FixAngle(GameObject toFix){
 		switch ((int)toFix.transform.localEulerAngles.y) {
 			case 179:
 				toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
+				isRotating = false;
+				card = null;
 				break;
 		}
 
-		yield return new WaitForSeconds (0.23f);
+		yield return new WaitForSeconds (1f);
 	}
 
 	public void Move(GameObject btn){
-		while (btn.transform.localEulerAngles.y <= 180) {
-			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
-			btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
-			StartCoroutine(FixAngle(btn));
+		Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
+		btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
+		StartCoroutine(FixAngle(btn));
+	}
+
+	public void Change(GameObject btn){
+		card = btn;
+		isRotating = true;
+	}
+
+	void Update(){
+		if(isRotating){
+			Move(card);
 		}
 	}
 }
