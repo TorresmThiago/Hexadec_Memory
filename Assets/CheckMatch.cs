cfe47c1c5b3191ebaac68980b9c4adc9666b77fc
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-17 23:56:11 -0300

    Do they match?
    You may be wondering why haven't I done all the other stuff. Well, if
    you figure this out, please let me know.
    We now heve the script that goes through the disabled text of both
    buttons and see if they match, returning true or false.

---
+++ Assets/CheckMatch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CheckMatch : MonoBehaviour {
+
+	string returnHex(GameObject btn){
+		//Lembrar: As cartas irao ter o nome com um index na frente: Col(or) ou Cod(e)  EX: Col92AF2B == Cod92AF2B
+		string hex = btn.GetComponentInChildren<Text> ().text.Substring (3);
+		return hex;
+	}
+
+	bool check(GameObject btn_a, GameObject btn_b){
+		string hex_a = returnHex (btn_a);
+		string hex_b = returnHex (btn_b);
+
+		if (hex_a == hex_b) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
