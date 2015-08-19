d851931f789295f8f74fd045cee9f4abd2d38b04
Thiago Torres <TheThiagoo0@gmail.com>
2015-08-19 10:11:55 -0300

    Fixed Cards.
    Fully implemented comparing cards mechanic. FINALLY.

--- Assets/RotationScript.cs
+++ Assets/RotationScript.cs
@@ -7,8 +7,8 @@ public class RotationScript : MonoBehaviour {
 	private GameObject card_1 = null;
 	private GameObject card_2 = null;
 	private string direction;
+	private bool hasCalled = false;
 
-	//Funçao chamada ao clique do button
 	public void Change(GameObject btn){
 		if (card_1 == null) {
 			card_1 = btn;
@@ -23,6 +23,7 @@ public class RotationScript : MonoBehaviour {
 		switch ((int)toFix.transform.localEulerAngles.y) {
 		case 179:
 			toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
+			hasCalled = true;
 			break;
 			
 		case 359:
@@ -30,8 +31,7 @@ public class RotationScript : MonoBehaviour {
 			break;
 		}
 	}
-	
-	//Funçao que necessariamente move as cartas
+
 	public void Move(){
 		if ((card_1 != null) && (card_1.transform.localEulerAngles.y != 180)) {
 			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
@@ -43,13 +43,12 @@ public class RotationScript : MonoBehaviour {
 			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
 			card_2.transform.rotation = Quaternion.Slerp (card_2.transform.rotation, newRotation, 0.1f);
 			FixAngle(card_2);
-		} else if ((card_2 != null) && (card_2.transform.localEulerAngles.y == 180)) {
+		} else if (((card_2 != null) && (card_1 != null)) && (card_2.transform.localEulerAngles.y == 180) && (hasCalled)) {
 			StartCoroutine(CompareCard());
-			print("I'm here");
+			hasCalled = false;
 		}
 	}
 
-	//Funçao que necessariamente move as cartas de volta
 	public void MoveBack(){
 		if ((card_1 != null) && (card_1.transform.localEulerAngles.y != 0)) {
 			Quaternion newRotation = Quaternion.AngleAxis (0, Vector3.up);
@@ -68,27 +67,25 @@ public class RotationScript : MonoBehaviour {
 		}
 	}
 
-	//Da a simpatica esperada e depois volta(ou nao)
 	IEnumerator CompareCard(){
 		yield return new WaitForSeconds (1);
 		if (verify()) {
 			//Implementar fadeOut
 			Destroy (card_1);
 			Destroy (card_2);
+			direction = null;
 		} else {
 			direction = "Back";
 		}
 	}
 
-	//ve se as cartas tem o mesmo codigohex
 	bool verify(){
 		if (gameObject.GetComponent<CheckMatch> ().check (card_1, card_2))
 			return true;	
 		
 		return false;
 	}
-	
-	//Update. Necessario para a atualizaçao visual da rotaçao
+
 	void Update(){
 		if (direction == "Front") {
 			Move();
