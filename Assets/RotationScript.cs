b3282cd43f29348e6fbca55535b89e70dc59ed5d
ThiagoZx <TheThiagoo0@gmail.com>
2015-08-19 01:59:20 -0300

    Yeah, it crashes.
    But at leaast, has the comparing mechanic. and WORKING!

--- Assets/RotationScript.cs
+++ Assets/RotationScript.cs
@@ -4,64 +4,97 @@ using System.Collections;
 
 public class RotationScript : MonoBehaviour {
 
-	private string isRotating = null;
-	private GameObject card;
-	private GameObject card_2;
+	private GameObject card_1 = null;
+	private GameObject card_2 = null;
+	private string direction;
 
-
-	void FixAngle(GameObject toFix){
-		switch ((int)toFix.transform.localEulerAngles.y) {
-			case 179:
-				toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
-				//isRotating = null;
-				//card = null;
-				break;
-
-			case 359:
-				toFix.transform.rotation = Quaternion.Euler (0, 0, 0);
-				isRotating = null;
-				//card = null;
-				break;
-		}
-	}
-
-	public void Move(GameObject btn){
-		Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
-		btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
-		FixAngle (btn);
-	}
-
-	public void MoveBack(GameObject btn){
-		Quaternion newRotation = Quaternion.AngleAxis (0, Vector3.up);
-		btn.transform.rotation = Quaternion.Slerp (btn.transform.rotation, newRotation, 0.1f);
-		FixAngle (btn);
-	}
-
+	//Funçao chamada ao clique do button
 	public void Change(GameObject btn){
-		if (card == null) {
-			card = btn;
-		} else {
+		if (card_1 == null) {
+			card_1 = btn;
+		} else if(card_2 == null){
 			card_2 = btn;
 		}
 
-		isRotating = "Front";
+		direction = "Front";
 	}
 
-	bool verify(GameObject btn_a, GameObject btn_b){
-		if (gameObject.GetComponent<CheckMatch> ().check (btn_a, btn_b)) {
-			return true;	
+	void FixAngle(GameObject toFix){
+		switch ((int)toFix.transform.localEulerAngles.y) {
+		case 179:
+			toFix.transform.rotation = Quaternion.Euler (0, 180, 0);
+			break;
+			
+		case 359:
+			toFix.transform.rotation = Quaternion.Euler (0, 0, 0);
+			break;
+		}
+	}
+	
+	//Funçao que necessariamente move as cartas
+	public void Move(){
+		if ((card_1 != null) && (card_1.transform.localEulerAngles.y != 180)) {
+			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
+			card_1.transform.rotation = Quaternion.Slerp (card_1.transform.rotation, newRotation, 0.1f);
+			FixAngle(card_1);
 		}
 
+		if ((card_2 != null) && (card_2.transform.localEulerAngles.y != 180)) {
+			Quaternion newRotation = Quaternion.AngleAxis (180, Vector3.down);
+			card_2.transform.rotation = Quaternion.Slerp (card_2.transform.rotation, newRotation, 0.1f);
+			FixAngle(card_2);
+		} else if ((card_2 != null) && (card_2.transform.localEulerAngles.y == 180)) {
+			StartCoroutine(CompareCard());
+			print("I'm here");
+		}
+	}
+
+	//Funçao que necessariamente move as cartas de volta
+	public void MoveBack(){
+		if ((card_1 != null) && (card_1.transform.localEulerAngles.y != 0)) {
+			Quaternion newRotation = Quaternion.AngleAxis (0, Vector3.up);
+			card_1.transform.rotation = Quaternion.Slerp (card_1.transform.rotation, newRotation, 0.1f);
+			FixAngle(card_1);
+		}
+		
+		if ((card_2 != null) && (card_2.transform.localEulerAngles.y != 0)) {
+			Quaternion newRotation = Quaternion.AngleAxis (0, Vector3.up);
+			card_2.transform.rotation = Quaternion.Slerp (card_2.transform.rotation, newRotation, 0.1f);
+			FixAngle(card_2);
+		} else if ((card_2 != null) && (card_2.transform.localEulerAngles.y == 0)) {
+			card_1 = null;
+			card_2 = null;
+			direction = null;
+		}
+	}
+
+	//Da a simpatica esperada e depois volta(ou nao)
+	IEnumerator CompareCard(){
+		yield return new WaitForSeconds (1);
+		if (verify()) {
+			//Implementar fadeOut
+			Destroy (card_1);
+			Destroy (card_2);
+		} else {
+			direction = "Back";
+		}
+	}
+
+	//ve se as cartas tem o mesmo codigohex
+	bool verify(){
+		if (gameObject.GetComponent<CheckMatch> ().check (card_1, card_2))
+			return true;	
+		
 		return false;
 	}
-
+	
+	//Update. Necessario para a atualizaçao visual da rotaçao
 	void Update(){
-		if(isRotating == "Front"){
-			Move(card);
-			if(card_2 != null)
-				Move(card_2);
-		} else if(isRotating == "Back"){
-			MoveBack(card);
+		if (direction == "Front") {
+			Move();
+		} else if(direction == "Back"){
+			MoveBack();
 		}
 	}
+
 }
