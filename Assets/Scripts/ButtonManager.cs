3c705d1019a87f4a489827265c76b76b0f33574e
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2015-08-26 20:04:58 -0300

    Gerenciamento de Telas
    -Menu com transição de telas;
    -Implementação dos créditos;

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void LoadScene(){
+		if (gameObject.tag == "Play") {
+			Application.LoadLevel (1);
+		} else if (gameObject.tag == "Credits") {
+			Application.LoadLevel (2);
+		} else if (gameObject.tag == "Menu") {
+			Application.LoadLevel (0);
+		}
+	}
+}
