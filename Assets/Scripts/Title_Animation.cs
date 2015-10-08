133ef16194193418f84207b5807e3cdf86cafe6d
Thiago Torres <TheThiagoo0@gmail.com>
2015-10-08 09:12:02 -0300

    Menu on!
    There are a feeew things left to do, not much tought. Just do it, would
    you?

--- Assets/Scripts/Title_Animation.cs
+++ Assets/Scripts/Title_Animation.cs
@@ -3,54 +3,20 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Title_Animation : MonoBehaviour {
-	
-	private bool Up = true;
-	private bool Down = false;
-	private float velocity = 0.023f;
-	public GameObject HexaText;
-	public GameObject MemoText;
+
 	public Camera MainCamera;
+	public GameObject Wave;
+	public float time;
 	private Color bgColor;
 	private bool onMenu;
 
-	void Awake(){
-		if (HexaText != null || MemoText != null)
-			onMenu = true;
-	}
-
 	void Start(){
-		if(onMenu)
-			StartCoroutine (IsMoving ());
 		StartCoroutine (ColorShifter ());
 	}
-
-	IEnumerator IsMoving(){
-		if (Up && !Down) {
-			HexaText.transform.position = new Vector3 (HexaText.transform.position.x, HexaText.transform.position.y + velocity, 0.0f);
-			MemoText.transform.position = new Vector3 (MemoText.transform.position.x, MemoText.transform.position.y + velocity, 0.0f);
-		}
-		if (Down && !Up) {
-			HexaText.transform.position = new Vector3 (HexaText.transform.position.x, HexaText.transform.position.y + (-velocity), 0.0f);
-			MemoText.transform.position = new Vector3 (MemoText.transform.position.x, MemoText.transform.position.y + (-velocity), 0.0f);
-		}
-
-		yield return new WaitForSeconds (0.023f);
-		StartCoroutine(IsMoving ());
-	}
-
-	void Update(){
-		if (onMenu) {
-			if (HexaText.transform.position.y >= 48/10) {
-				Down = true;
-				Up = false;
-			} else if(HexaText.transform.position.y <= 39/10){
-				Up = true;
-				Down = false;
-			}
-		}
-	}
-
+	
 	IEnumerator ColorShifter(){
+		yield return new WaitForSeconds (time);
+		Instantiate (Wave);
 		while (true){ 
 			bgColor = new Color(Random.value, Random.value, Random.value, 0.5f);
 
